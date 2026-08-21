cd883dfccb4611d404becef73abb8a85b91b5244
sqhil-a <sahilambegaonkar@gmail.com>
2026-08-21 17:27:22 -0400

    Complete commit functionality test

--- src/content/team.js
+++ src/content/team.js
@@ -4,7 +4,7 @@ export const nationalTeams = {
     path: "/team25-26",
     members: [
       {
-        name: "Sahil Bhatt",
+        name: "Shivam Bhatt",
         role: "Chief Executive Officer & President",
         profileRole: "Chief Executive Officer (CEO) & President",
         department: "National",
